ae4606852540765d6380cb8edbfb616113770522
clover1420 <47273274+clover1420@users.noreply.github.com>
2021-02-03 18:00:11 +0800

    1.1

--- js/autoload.js
+++ js/autoload.js
@@ -27,7 +27,7 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource("css/waifu.css", "css"),
+		loadExternalResource("https://cdn.jsdelivr.net/gh/clover1420/clover@master/css/waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
